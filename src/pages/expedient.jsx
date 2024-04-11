f777f2ef9a20017b1292387f2c76ef7439665865
Rodrigo Castellanos <rodrigojoaquinc89@gmail..com>
2024-04-11 12:58:12 -0300

    descarga de caratula

--- src/pages/expedient.jsx
+++ src/pages/expedient.jsx
@@ -1,32 +1,25 @@
 import React, { useEffect, useState } from "react";
-
 import { useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
-
 import useExpedients from "../hooks/useExpedients";
-
 import DocumentsTable from "../components/table/DocumentsTable";
-
 import { FaArrowAltCircleLeft } from "react-icons/fa";
-
 import { MdDownload } from "react-icons/md";
-
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
 import "../styles/expedient.css";
+import axios from "axios";
 
 const Expedient = () => {
   const [expedient, setExpedient] = useState({});
-
   const { tab, subTab } = useSelector((state) => state.tab);
   const { files } = useSelector((state) => state.files);
-
   const { expedientId } = useParams();
   const { getExpedient } = useExpedients();
-
   const navigate = useNavigate();
+
+  const BaseUrl = import.meta.env.VITE_API_URL;
 
   const handleComeBack = () => {
     navigate(`/${tab}/${subTab}`);
@@ -35,6 +28,25 @@ const Expedient = () => {
   useEffect(() => {
     getExpedient(setExpedient, expedientId);
   }, []);
+
+  const handleDownload = async () => {
+    try {
+      const response = await axios.get(
+        `${BaseUrl}expedientes/${expedientId}/caratula`,
+        {
+          responseType: "blob",
+        }
+      );
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "caratula.pdf");
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      console.error("Error al descargar el archivo:", error);
+    }
+  };
 
   return (
     <>
@@ -52,10 +64,8 @@ const Expedient = () => {
               className="button-back"
               onClick={handleComeBack}
             />
-
             <h1>Expediente N° {expedient.numero}</h1>
-
-            <MdDownload className="button-back" />
+            <MdDownload className="button-back" onClick={handleDownload} />
           </Col>
         </Row>
         <Row id="expedient-description">
@@ -73,14 +83,12 @@ const Expedient = () => {
             </ul>
           </Col>
           <Col ms={6}>
-            {" "}
             <ul>
               <li>
                 <strong> Fecha: </strong> {expedient.fechaCaratulacion}
               </li>
               <li>
-                <strong> Descripción: </strong>
-                {expedient.descripcion}
+                <strong> Descripción: </strong> {expedient.descripcion}
               </li>
             </ul>
           </Col>
